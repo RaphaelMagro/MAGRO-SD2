0c4384460491d9aa68b1c387e629a5a729d2c641
RaphaelMagro <raphaelmagro123@gmail.com>
2026-03-12 11:53:27 +0800

    Intro to JS

---
+++ session14/index.js
@@ -0,0 +1,73 @@
+// This is the JavaScript file for session 14. You can write your JavaScript code here.
+// console.log(stringContent|Variable)
+console.log("Hello, World!");
+
+// To use comment, we need to use -> //
+// This is a single line comment
+
+/*
+This is a multi-line comment
+We can write multiple lines of comments here
+*/
+
+// [SECTION] Variables
+// Acts as a storage for data. We can store different types of data in variables, such as numbers, strings, booleans, etc. Except constant variables.
+
+// [SECTION] Data Types
+// Numeric Data Types: Integer, Float, Double, etc.
+
+/*
+let variableName; -> no initialized value
+let variableName = value; -> with initialized value
+*/
+
+let age;
+let height = 160;
+
+console.log(height);
+// JS is a loose-type PL
+console.log(height)
+console.    log     (height)
+console
+.
+log
+(height)
+
+// Synchronous PL -> JS
+
+let gwa = 99.75;
+console.log(gwa);
+
+// Character data types
+
+let firstName = "John";
+console.log(firstName);
+
+// typeof -> to check a variable's type of data
+console.log(typeof gwa);
+console.log(typeof firstName);
+console.log(typeof height);
+
+// Text Casing in JS
+// camelCase -> firstName, lastName, etc. (commonly used in JS)
+// snake_case -> first_name, last_name, etc.
+// PascalCase -> FirstName, LastName, etc. (commonly used in C#)
+// Kebab-case -> first-name, last-name, etc. (commonly used in CSS)
+
+// Boolean Data Types
+let isMarried = false;
+let isRegistered = true;
+let hasPassed = true;
+
+console.log(isMarried);
+console.log(isRegistered);
+console.log(typeof isMarried);
+
+// Concatenation
+
+let province = "Pampanga";
+console.log("I live in " + province);
+
+// Type-coercion -> automatic conversion of data types
+// Concatenation of string and number -> string
+console.log("My overall grade is " + gwa);
